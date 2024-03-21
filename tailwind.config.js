cc79206d6ae52135fa6ebec6eb42532df61d1878
caarlosdamian <dapecarlos@gmail.com>
2024-03-21 17:44:07 -0600

    feat(styles): added base styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#D87D4A',
+        secondary: '#101010',
+        third: '#FBAF85',
+        customBlack: '#000',
+        customWhite: '#FFF',
+        customGray: '#F1F1F1',
+        customAlmostWhite: '#FAFAFA',
+      },
+    },
   },
   plugins: [],
 };
