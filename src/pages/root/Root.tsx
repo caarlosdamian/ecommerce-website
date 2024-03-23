6942f6a4c53aad917f8acce123dc8936d558e21c
caarlosdamian <dapecarlos@gmail.com>
2024-03-22 19:12:11 -0600

    refactor(styles): added some refactors into tailwind config

--- src/pages/root/Root.tsx
+++ src/pages/root/Root.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export const Root = () => {
   return (
-    <div className="bg-red-500">
+    <section className="bg-red-500">
       <Outlet />
-    </div>
+    </section>
   );
 };
