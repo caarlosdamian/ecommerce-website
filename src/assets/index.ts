273f5353843f8f65fd41f6042cecf3eec99f60e7
caarlosdamian <dapecarlos@gmail.com>
2024-03-28 10:50:19 -0600

    feat(header): added header component

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+import cart from './shared/desktop/icon-cart.svg';
+import logo from './shared/desktop/logo.svg';
+import iconArrowRight from './shared/desktop/icon-arrow-right.svg';
+import facebook from './shared/desktop/icon-facebook.svg';
+import hamburger from './shared/tablet/icon-hamburger.svg'
+
+
+export {
+    cart,
+    logo,
+    facebook,
+    iconArrowRight,
+    hamburger
+}
--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,7 +1,19 @@
+import { cart, hamburger, logo } from "../../../assets";
+
 export const Header = () => {
   return (
-    <header className="">
-      <div className="container mx-auto backdrop:flex justify-between"></div>
+    <header className="bg-customBlack h-[89px]">
+      <section className="container mx-auto flex justify-between items-center h-full px-6">
+        <div className="">
+          <img src={hamburger} alt="hamburger" />
+        </div>
+        <div className="">
+        <img src={logo} alt="logo" />
+        </div>
+        <div className="">
+        <img src={cart} alt="cart" />
+        </div>
+      </section>
     </header>
   );
 };
