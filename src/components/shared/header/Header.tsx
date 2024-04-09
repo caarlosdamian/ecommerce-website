e059be9d8a6a342be342f59e09df3b2bf3dd59c1
caarlosdamian <dapecarlos@gmail.com>
2024-04-08 23:48:13 -0600

    feat(components): added some components fixes

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
   const { formatMessage } = useIntl();
   const { handleOpenModal} = useModalContext();
   return (
-    <header className="bg-customBlack h-[89px] border-b border-customWhite sm:border-none border-opacity-[0.104] lg:h-[96px] sm:px-10">
+    <header className="bg-customBlack h-[89px] border-b border-customWhite sm:border-none border-opacity-[0.104] lg:h-[96px] sm:px-10 relative z-20">
       <section className="container mx-auto flex justify-between items-center h-full w-full px-6 gap-[42px] border-opacity-[0.104] sm:border-customWhite sm:border-b sm:border-opacity-20 md:px-0">
         <div className="lg:hidden">
           <img
