34f58a95a31d3e14829cb25be1f5b28979bfaeec
caarlosdamian <dapecarlos@gmail.com>
2024-05-19 13:37:54 -0600

    feat(product): added categories to product page

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,16 +1,23 @@
 import { useLocation } from 'react-router-dom';
 import { ProductI } from '../../types';
 import { Mosaic } from '../../components/shared/mosaic/Mosaic';
+import { categories } from '../../constants';
+import { CategoryCard } from '../../components';
 
 export const Product = () => {
   const { state } = useLocation();
   const {
     product: { gallery },
   } = state as { product: ProductI };
-  console.log('gallery', gallery);
+  // remove py-8
   return (
     <div className="container max-w-[1110px] mx-auto py-8 px-6 flex flex-col gap-[120px]">
       <Mosaic images={gallery} />
+      <div className="px-6 pt-[100px] pb-[120px] flex flex-col items-center gap-[68px] sm:flex-row sm:gap-3 sm:justify-center sm:px-10 sm:pb-24 lg:pt-[200px] lg:gap-8 lg:pb-[168px]">
+        {categories.map(({ id, img, path }) => (
+          <CategoryCard category={id} path={path} productImg={img} key={id} />
+        ))}
+      </div>
     </div>
   );
 };
