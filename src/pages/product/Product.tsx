7c91cdf9756e0a52586553d4353f2d2b7b7cf821
caarlosdamian <dapecarlos@gmail.com>
2024-06-10 23:20:54 -0600

    feat(product): added product items

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -12,6 +12,40 @@ export const Product = () => {
 
   return (
     <div className="container max-w-[1110px] mx-auto py-8 px-6 flex flex-col gap-[120px]">
+      <div className="">
+        <div className=""></div>
+        {/* details */}
+        <div className="flex flex-col gap-20 lg:flex-row lg:gap-32">
+          <div className="flex flex-col gap-6 flex-1">
+            <h3 className="h3-bold uppercase">FEATURES</h3>
+            <p className="body opacity-50">
+              Featuring a genuine leather head strap and premium earcups, these
+              headphones deliver superior comfort for those who like to enjoy
+              endless listening. It includes intuitive controls designed for any
+              situation. Whether you’re taking a business call or just in your
+              own personal space, the auto on/off and pause features ensure that
+              you’ll never miss a beat. The advanced Active Noise Cancellation
+              with built-in equalizer allow you to experience your audio world
+              on your terms. It lets you enjoy your audio in peace, but quickly
+              interact with your surroundings when you need to. Combined with
+              Bluetooth 5. 0 compliant connectivity and 17 hour battery life,
+              the XX99 Mark II headphones gives you superior sound, cutting-edge
+              technology, and a modern design aesthetic.
+            </p>
+          </div>
+          <div className="flex-1 flex flex-col gap-6 sm:flex-row sm:justify-between sm:pr-[180px] lg:pr-0 lg:flex-col lg:justify-normal">
+            <h3 className="h3-bold uppercase">in the box</h3>
+            <div className="flex flex-col gap-2">
+              {product.includes.map(({ item, quantity }) => (
+                <div className="flex gap-6" key={item}>
+                  <p className="body text-primary font-bold">{quantity}x</p>
+                  <p className="body opacity-50">{item}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
       <Mosaic images={product.gallery} />
       {/* Other products */}
       <div className="grid grid-cols-1 gap-14 sm:grid-cols-3 sm:gap-3 lg:gap-8">
